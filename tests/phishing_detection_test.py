eaf41c52833887723a66542afe3520c9a48c0019
rozhn0v <rozhnov@alignedresearch.com>
2020-06-25 20:12:21 +0400

    add tests for get_lang_for_ip function

--- tests/phishing_detection_test.py
+++ tests/phishing_detection_test.py
@@ -1,4 +1,8 @@
 import unittest
+from unittest.mock import Mock
+from unittest.mock import PropertyMock
+
+from ipaddress import IPv4Address
 
 import idn_domain_names.phishing_detection as pd
 from idn_domain_names.domain import Domain
@@ -11,3 +15,33 @@ class PhishingDetectionTest(unittest.TestCase):
         actual = next(actual)
         expected = (punycode, Domain('bücher.tld.'))
         self.assertEqual(expected, actual)
+
+    def test_get_lang_by_ip_when_response_is_none(self):
+        lib = Mock()
+        lib.map.side_effect = [[None]]
+        actual = pd.get_lang_by_ip([IPv4Address('1.2.3.4')], lib)
+        self.assertEqual([None], actual)
+        lib.get.assert_called()
+
+    def test_get_lang_by_ip_when_content_is_none(self):
+        response = Mock()
+        content = PropertyMock(return_value='')
+        type(response).content = content
+
+        lib = Mock()
+        lib.map = Mock(return_value=[response])
+
+        actual = pd.get_lang_by_ip([IPv4Address('1.2.3.4')], lib)
+        self.assertEqual([None], actual)
+        content.assert_called()
+
+    def test_get_lang_by_ip(self):
+        response = Mock()
+        content = PropertyMock(return_value='<html lang="ru"></html>')
+        type(response).content = content
+
+        lib = Mock()
+        lib.map = Mock(return_value=[response])
+
+        actual = pd.get_lang_by_ip([IPv4Address('1.2.3.4')], lib)
+        self.assertEqual(['ru'], actual)
