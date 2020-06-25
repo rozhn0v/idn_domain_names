c4e9396b980c73db7028d6525fe6528d790155b7
rozhn0v <rozhnov@alignedresearch.com>
2020-06-26 02:29:40 +0400

    unit test for detect_phishing

--- tests/phishing_detection_test.py
+++ tests/phishing_detection_test.py
@@ -1,8 +1,6 @@
 import unittest
-from unittest.mock import Mock
-from unittest.mock import PropertyMock
-
 from ipaddress import IPv4Address
+from unittest.mock import ANY, Mock, PropertyMock, patch
 
 import idn_domain_names.phishing_detection as pd
 from idn_domain_names.domain import Domain
@@ -45,3 +43,21 @@ class PhishingDetectionTest(unittest.TestCase):
 
         actual = pd.get_lang_by_ip([IPv4Address('1.2.3.4')], lib)
         self.assertEqual(['ru'], actual)
+
+    @patch('idn_domain_names.phishing_detection._is_homoglyph_domain_valid')
+    @patch('idn_domain_names.filesystem.report_phishing')
+    def test_detect_phishing(self, report_phishing, domain_validation):
+        phishing = Domain('xn--bcher-kva.tld.')
+        delegate = {phishing: ('0.0.0.0', 1)}
+        table = Mock()
+        table.get_ip_and_asn = lambda k: delegate[k]
+
+        domain_validation.return_value = -1
+
+        pd.detect_phishing(domains_to_check=[phishing],
+                           ip_table=table,
+                           phishing_targets={Domain('bucher.tld.')},
+                           path_to_output='unused')
+
+        report_phishing.assert_called_with(phishing, ANY)
+        self.assertTrue(domain_validation.mock_calls)
