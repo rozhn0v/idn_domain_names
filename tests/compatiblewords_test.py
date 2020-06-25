180bd3f92e6dac3be1eb9dd47f118d87ea7187f3
rozhn0v <rozhnov@alignedresearch.com>
2020-06-25 17:10:14 +0400

    create test stub for TextBlob

--- tests/compatiblewords_test.py
+++ tests/compatiblewords_test.py
@@ -1,8 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
 
-from textblob import TextBlob
-
 import idn_domain_names.compatiblewords as cw
 from idn_domain_names.compatiblewords import CompatibleWords
 from idn_domain_names.compatiblewords import Words
@@ -17,7 +15,7 @@ class WordsTest(unittest.TestCase):
 
         words = Words(['hrse', 'wyne'], spell_checker=cons)
         expected = Words(['horse', 'wine'])
-        self.assertEqual(expected, words.spellcheck_words('en'))
+        self.assertEqual(expected, words.spellcheck('en'))
 
 
 class CompatibleWordsTest(unittest.TestCase):
@@ -27,12 +25,35 @@ class CompatibleWordsTest(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_check_compatibility(self):
-        word = TextBlob('frasescélebres')
-        homoglyph = TextBlob('frasescelebres')
+        word = TextBlobStub('frasescélebres', 'es', 'frases célebres')
+        homoglyph = TextBlobStub('frasescelebres', 'pt')
+        print(word.detect_language())
         self.assertTrue(CompatibleWords(word, homoglyph).check_compatibility())
 
     def test_check_compatibility_with_extra_symbol(self):
-        word = TextBlob('gmailç')
-        homoglyph = TextBlob('gmail')
+        word = TextBlobStub('gmailç', 'fr')
+        homoglyph = TextBlobStub('gmail', 'en')
         words = CompatibleWords(word, homoglyph)
         self.assertFalse(words.check_compatibility())
+
+
+class TextBlobStub:
+    def __init__(self, word, lang, translation=None):
+        self.word = word
+        self.lang = lang
+        self.translation = translation
+
+    def __len__(self):
+        return len(self.word)
+
+    def detect_language(self):
+        return self.lang
+
+    def translate(self, from_lang, to):
+        return self.translation
+
+    def __repr__(self):
+        return '{} {} {}'.format(self.word, self.lang, self.translation)
+
+    def __str__(self) -> str:
+        return self.word
